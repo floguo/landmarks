852b4b95bea7f0799fd160435a2602759ab6b68f
floguo <floraguolr@gmail.com>
2025-01-09 00:25:58 -0700

    start user profile, working with UI controls

---
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Flora Guo on 2025-01-09.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ProfileHost()
+}
---
+++ Landmarks/Views/Profiles/ProfileSummary.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileSummary.swift
+//  Landmarks
+//
+//  Created by Flora Guo on 2025-01-09.
+//
+
+import SwiftUI
+
+struct ProfileSummary: View {
+    var profile: Profile
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 10) {
+                Text(profile.username)
+                    .bold()
+                    .font(.title)
+                
+                Text("Notifications: \(profile.prefersNotifications ? "On": "Off")")
+                Text("Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
+                Text("Goal Date: ") + Text(profile.goalDate, style: .date)
+            }
+        }
+    }
+}
+
+#Preview {
+    ProfileSummary(profile: Profile.default)
+}
