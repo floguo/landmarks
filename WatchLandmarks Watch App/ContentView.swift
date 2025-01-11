29ce6e50b0aba0f680612569365f18c32a1a96a2
floguo <floraguolr@gmail.com>
2025-01-11 16:49:42 -0500

    add watchOS app

---
+++ WatchLandmarks Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Flora Guo on 2025-01-11.
+//
+
+import SwiftUI
+import UserNotifications
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .task {
+                let center = UNUserNotificationCenter.current()
+                _ = try? await center.requestAuthorization(
+                    options: [.alert, .sound, .badge]
+                )
+            }
+    }
+}
+
+#Preview {
+    ContentView()
+        .environment(ModelData())
+}
